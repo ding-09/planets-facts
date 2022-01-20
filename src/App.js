d1d37829368ba5831d881a5caf689eaeb0eb381f
Tiffany <yang.ding@hotmail.com>
2022-01-19 18:45:45 -0800

    wrap App in routes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 import GlobalStyles from './GlobalStyle';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import planetsData from './data.json';
 import Header from './components/Header/Header';
-import bgImg from './assets/background-stars.svg';
 
 function App() {
-  // json data for planets
-  const [planets, setPlanets] = useState(planetsData);
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <main>
-        <Header planets={planets} />
-      </main>
+      <Router>
+        <main>
+          <Header planets={planetsData} />
+        </main>
+        <Routes>
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
